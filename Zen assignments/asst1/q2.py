53107f5cb0284b01074f653a1bb8be662dbc8c69
Shafaaf Hossain <shafaafhossain@shafaafMacbook.local>
2017-03-28 05:49:30 -0400

    Sd

--- Zen assignments/asst1/q2.py
+++
@@ -1,4 +0,0 @@
-def rotate_word(str, i):
-    for j in xrange(0, i):
-        str = str[1:] + str[0]
-    return str
--- Zen assignments/asst1/q5.py
+++
@@ -1,4 +0,0 @@
-import math
-
-def Q(D):
-    return map(lambda d: int(math.sqrt((2 * 50 * d)/30)), D)
